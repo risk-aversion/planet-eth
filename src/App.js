8f8a9b07c8250f2f8a9b3c8899b7d59dad7d4ac6
apguan <apguan@gmail.com>
2021-09-22 01:02:24 -0700

    adding baseui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Client as Styletron } from "styletron-engine-monolithic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider, styled } from "baseui";
+import logo from "./logo.svg";
 
-function App() {
+const engine = new Styletron();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </BaseProvider>
+    </StyletronProvider>
   );
-}
+};
 
 export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+const staging =
+  "https://api-staging.rarible.com/protocol/v0.1/ethereum/nft/items/byCollection?collection=0x8e3ab56dff9ad1954c8cb7e8890ba049ca4adf13&includeMeta=true";
