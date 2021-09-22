c74061e3e2b36d76f6e65edd8caac9a03e4a8777
apguan <apguan@gmail.com>
2021-09-22 00:49:33 -0700

    scaffolding public site

---
+++ src/utils/color-generator.js
@@ -0,0 +1,14 @@
+export const stringToColor = (str) => {
+  let hash = 0;
+  for (const i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+  for (const i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ("00" + value.toString(16)).substr(-2);
+  }
+
+  return color;
+};
